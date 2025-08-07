bdab1671b11f2c1abb73dca60386aca83a17c37f
Tim Götzelmann <72736935+TLTimeplex@users.noreply.github.com>
2025-08-07 12:48:22 +0200

    Fix favicon header

--- app/app.vue
+++ app/app.vue
@@ -6,4 +6,11 @@
 
 <script setup>
 // Theme wird automatisch durch das Plugin initialisiert
+
+useHead({
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    { rel: 'shortcut icon', href: '/favicon.ico' }
+  ]
+})
 </script>

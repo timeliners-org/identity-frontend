726a89fc7415bf384ace65b595053ee858b5195c
Tim Götzelmann <72736935+TLTimeplex@users.noreply.github.com>
2025-08-08 16:28:31 +0200

    Updated Title set algorithm

--- app/app.vue
+++ app/app.vue
@@ -5,13 +5,10 @@
 </template>
 
 <script setup lang="ts">
-const route = useRoute();
-
 useHead({
   link: [
     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     { rel: 'shortcut icon', href: '/favicon.ico' }
   ],
-  meta: [{ property: "og:title", content: `App Name - ${route.meta.title}` }],
 })
 </script>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -10,7 +10,10 @@
 </template>
 
 <script lang="ts" setup>
-
+const route = useRoute();
+useHead({
+  meta: [{ property: "og:title", content: `App Name - ${route.meta.title}` }],
+});
 </script>
 
 <style>
